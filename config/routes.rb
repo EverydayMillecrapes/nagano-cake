72347fe715460e3b136cbff5083ef94f471f8072
EC2 Default User <ec2-user@ip-172-31-52-115.ec2.internal>
2021-06-17 09:20:40 +0000

    routing設定したよ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+   root to: 'homes#top'
+  get  "home/about"  => "homes#about"
+  
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers,only: [:show,:index,:edit,:update]
+  get "customers/quit" => "customers#quit"
+  get "customers/out" => "customers#out"
+  
+  resources :cart_products,only:[:index,:update,:create,:destroy]
+  delete 'cart_products' => 'all_destroy'
+  resources :products,only: [:show,:index]
+  resources :orders, only: [:new, :create, :index, :show]
+  post 'orders/log' => 'customers#log'
+  get 'orders/thanx' => 'orders#thanx'
+  resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+  
+  namespace :admin do
+    devise_for :customers
+    resources :customers, only: [:index,:show, :edit,:update]
+    resources :products, only: [:index,:show, :edit,:update, :new, :create]
+    resources :genres, only: [:index,:create, :edit,:update]
+    resources :orders, only: [:index, :show, :update]
+  end	
 end
