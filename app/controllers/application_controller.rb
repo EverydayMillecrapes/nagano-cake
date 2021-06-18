4dd1d3d5c698f3cd88a66451c3b537e072cdcb9e
EC2 Default User <ec2-user@ip-172-31-52-112.ec2.internal>
2021-06-18 03:05:45 +0000

    顧客ログイン実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def after_sign_in_path_for(resource)
+      if customer_signed_in?
+        customers_path(resource)
+      else
+        admin_top_path
+      end
+    end
+
+    def after_sign_out_path_for(resource)
+      root_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,
+  			 keys: [:first_name, :last_name, :kana_first_name, :kana_last_name,
+                :email, :post_code, :address, :phone_number])
+
+
+  		devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+
+    end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def set_cart_item
+    @cart_item = CartItem.find(params[:id])
+  end
+
 end
