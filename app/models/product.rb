bf2c0d74a55ef7ac3b786a345512f1a5b2a4dd62
EC2 Default User <ec2-user@ip-172-31-12-74.ec2.internal>
2021-06-23 10:05:14 +0000

    Add_products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
 	has_many :cart_products
 	has_many :order_products
-	    belongs_to :genre
+	belongs_to :genre
 
 	attachment :image
 # 	enum status: [:販売可, :販売不可]
