3081f3ea790d6eb349a321c1ed66f9fbaffd102c
EC2 Default User <ec2-user@ip-172-31-52-115.ec2.internal>
2021-06-23 10:18:38 +0000

    商品編集、一覧、詳細、新規登録

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,37 @@
 class Admin::ProductsController < ApplicationController
+    def new
+     @product = Product.new
+    end
+    
+    def index
+     @products = Product.all
+    end
+    
+    def create
+    @product= Product.new(product_params)
+    @product.save
+    redirect_to admin_product_path(@product)
+    end
+    
+    def show
+    @product = Product.find(params[:id])
+    end
+    
+    def edit
+    @product = Product.find(params[:id])
+    end
+
+    def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+        redirect_to admin_product_path(@product), notice: "You have updated genre successfully."
+    else
+        render "edit"
+    end
+    end
+    
+    def product_params
+    params.require(:product).permit(:name, :non_tax_price, :introduction, :is_active, :genre_id, :image)
+    end
+    
 end
